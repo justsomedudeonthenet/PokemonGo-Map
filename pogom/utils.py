94bdb10c9df2a8ba53c7655b28d00cdcfe630a03
Justsomedudeonthenet <fake@email.addr>
2016-08-23 11:03:47 -0400

    Create queues for accounts
    This allows you to specify more accounts than you have search workers.
    
    When a search worker fails and reinitializes it's API, it gets a new account.
    This is especially useful for upcoming queueing methods where a single worker is responsible for an area.  With this method, if that worker is banned it can switch accounts instead of failing to scan it's assigned area.

--- pogom/utils.py
+++ pogom/utils.py
@@ -52,6 +52,8 @@ def get_args():
                         help='Usernames, one per account.')
     parser.add_argument('-p', '--password', action='append',
                         help='Passwords, either single one for all accounts or one per account.')
+    parser.add_argument('-mw', '--max-workers', type=int,
+                        help='Maximum search worker threads to start. Defaults to the number of accounts specified.')
     parser.add_argument('-l', '--location', type=parse_unicode,
                         help='Location, can be an address or coordinates')
     parser.add_argument('-j', '--jitter', help='Apply random -9m to +9m jitter to location',
@@ -219,6 +221,10 @@ def get_args():
         # Make the accounts list
         for i, username in enumerate(args.username):
             args.accounts.append({'username': username, 'password': args.password[i], 'auth_service': args.auth_service[i]})
+
+        # Make max workers equal number of accounts if unspecified
+        if (args.max_workers is None):
+            args.max_workers = len(args.accounts)
 
     return args
 
