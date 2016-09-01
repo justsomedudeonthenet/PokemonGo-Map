ee76af079e0dc27206fe0fa2d8c3397378a70b30
Justsomedudeonthenet <fake@email.addr>
2016-08-31 21:34:59 -0400

    Allow disabling search interval, disable by default

--- pogom/utils.py
+++ pogom/utils.py
@@ -54,8 +54,8 @@ def get_args():
                         help='Passwords, either single one for all accounts or one per account.')
     parser.add_argument('-w', '--workers', type=int,
                         help='Number of search worker threads to start. Defaults to the number of accounts specified.')
-    parser.add_argument('-asi', '--account-search-interval', type=int, default=28800,
-                        help='Seconds for accounts to search before switching to a new account')
+    parser.add_argument('-asi', '--account-search-interval', type=int, default=0,
+                        help='Seconds for accounts to search before switching to a new account. 0 to disable.')
     parser.add_argument('-ari', '--account-rest-interval', type=int, default=7200,
                         help='Seconds for accounts to rest when they fail or are switched out')
     parser.add_argument('-l', '--location', type=parse_unicode,
@@ -229,6 +229,10 @@ def get_args():
         # Make max workers equal number of accounts if unspecified, and disable account switching
         if (args.workers is None):
             args.workers = len(args.accounts)
+            args.account_search_interval = None
+
+        # Disable search interval if 0 specified
+        if args.account_search_interval == 0:
             args.account_search_interval = None
 
     return args
