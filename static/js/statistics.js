78964a611099a542d0a460532ec5a274d18a8926
Aldarund <shaper.int@gmail.com>
2016-09-02 17:19:08 +0300

    optimize amount of data transfered when loading all location in the stat

--- static/js/statistics.js
+++ static/js/statistics.js
@@ -185,7 +185,8 @@ function updateMap (firstRun) {
 updateMap()
 
 /* Overlay */
-var detailsLoading = false
+var detailsLoading = false;
+var appearancesTimesLoading = false;
 var detailInterval = null
 var lastappearance = 1
 var pokemonid = 0
@@ -225,10 +226,42 @@ function loadDetails () {
   })
 }
 
+function loadAppearancesTimes(pokemon_id, spawnpoint_id) {
+  return $.ajax({
+    url: 'raw_data',
+    type: 'GET',
+    data: {
+      'pokemon': false,
+      'pokestops': false,
+      'gyms': false,
+      'scanned': false,
+      'appearances': false,
+      'appearancesDetails': true,
+      'pokemonid': pokemon_id,
+      'spawnpoint_id': spawnpoint_id,
+      'duration': $('#duration').val()
+    },
+    dataType: 'json',
+    beforeSend: function () {
+      if (appearancesTimesLoading) {
+        return false
+      } else {
+        appearancesTimesLoading = true
+      }
+    },
+    complete: function () {
+      appearancesTimesLoading = false
+    }
+  })
+}
+
 function showTimes (marker) {
-  var uuid = marker.position.lat().toFixed(7) + '_' + marker.position.lng().toFixed(7)
-  $('#times_list').html(appearanceTab(mapData.appearances[uuid]))
-  $('#times_list').show()
+  appearanceTab(mapData.appearances[marker.spawnpoint_id]).then(function (value) {
+    console.log(value);
+    $('#times_list').html(value);
+    $('#times_list').show();
+  });
+
 }
 
 function closeTimes () {
@@ -373,27 +406,16 @@ function closeOverlay () { // eslint-disable-line no-unused-vars
 }
 
 function processAppearance (i, item) {
-  var saw = new Date(item['disappear_time'])
-  saw = saw.getHours() + ':' +
-  ('0' + saw.getMinutes()).slice(-2) + ':' +
-  ('0' + saw.getSeconds()).slice(-2) + ' ' +
-  saw.getDate() + ' ' +
-  monthArray[saw.getMonth()] + ' ' +
-  saw.getFullYear()
-  var uuid = item['latitude'].toFixed(7) + '_' + item['longitude'].toFixed(7)
-  if (!((uuid) in mapData.appearances)) {
+  var spawnpoint_id = item['spawnpoint_id'];
+  if (!((spawnpoint_id) in mapData.appearances)) {
     if (item['marker']) {
       item['marker'].setMap(null)
     }
-    item['count'] = 1
-    item['times'] = [saw]
-    item['uuid'] = uuid
-    item['marker'] = setupPokemonMarker(item, true)
-
-    mapData.appearances[item['uuid']] = item
+    item['marker'] = setupPokemonMarker(item, true);
+    item['marker'].spawnpoint_id = spawnpoint_id;
+    mapData.appearances[spawnpoint_id] = item
   } else {
-    mapData.appearances[uuid].count++
-    mapData.appearances[uuid].times.push(saw)
+    mapData.appearances[spawnpoint_id].count += item['count'];
   }
 
   heatmapPoints.push(new google.maps.LatLng(item['latitude'], item['longitude']))
@@ -402,23 +424,30 @@ function processAppearance (i, item) {
 
 function redrawAppearances (appearances) {
   $.each(appearances, function (key, value) {
-    var item = appearances[key]
+    var item = appearances[key];
     if (!item['hidden']) {
-      var newMarker = setupPokemonMarker(item, true)
-      item['marker'].setMap(null)
+      var newMarker = setupPokemonMarker(item, true);
+      item['marker'].setMap(null);
+      item['marker'].spawnpoint_id = spawnpoint_id;
       appearances[key].marker = newMarker
     }
   })
 }
 
 function appearanceTab (item) {
-  var times = ''
-
-  $.each(item['times'], function (key, value) {
-    times = '<div class="row' + (key % 2) + '">' + value + '</div>' + times
-  })
-
-  return `<div>
+  var times = '';
+  return loadAppearancesTimes(item['pokemon_id'], item['spawnpoint_id']).then(function (result) {
+    $.each(result.appearancesTimes, function(key, value){
+      var saw = new Date(value);
+      saw = saw.getHours() + ':' +
+          ('0' + saw.getMinutes()).slice(-2) + ':' +
+          ('0' + saw.getSeconds()).slice(-2) + ' ' +
+          saw.getDate() + ' ' +
+          monthArray[saw.getMonth()] + ' ' +
+          saw.getFullYear();
+      times = '<div class="row' + (key % 2) + '">' + saw + '</div>' + times
+    });
+    return `<div>
                 <a href="javascript:closeTimes();">Close this tab</a>
             </div>
             <div class="row1">
@@ -434,11 +463,12 @@ function appearanceTab (item) {
             <div>
                 ${times}
             </div>`
+  });
 }
 
 function updateDetails () {
   loadDetails().done(function (result) {
-    $.each(result.appearances, processAppearance)
+    $.each(result.appearances, processAppearance);
 
     // Redraw the heatmap with all the new appearances
     if (heatmapNumPoints !== heatmapPoints.length) {
@@ -449,11 +479,13 @@ function updateDetails () {
         data: heatmapPoints,
         map: map,
         radius: 50
-      })
+      });
       heatmapNumPoints = heatmapPoints.length
     }
   })
 }
+
+
 
 if (location.href.match(/overlay_[0-9]+/g)) {
   showOverlay(location.href.replace(/^.*overlay_([0-9]+).*$/, '$1'))
